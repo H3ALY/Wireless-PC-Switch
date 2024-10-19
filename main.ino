81b60ce0cb9708d6e059eccc350c51c1e2826204
Ross 'H3ALY' Healy <ross_healy@live.co.uk>
2024-10-19 09:31:45 +0100

    Update paths and additional otaManager functionality

--- main.ino
+++ main.ino
@@ -1,14 +1,19 @@
 #include <SimpleTimer.h>
-#include "wifi_setup.h"
-#include "mqtt_handler.h"
-#include "power_control.h"
-#include "ota_setup.h"
 #include "config.h"
+#include "core/wifi_setup.h"
+#include "core/mqtt_handler.h"
+#include "core/power_control.h"
+
+#if enable_ota
+#include "core/ota_setup.h"
+#endif
 
 WiFiClient espClient;
 MqttClient mqttClient(espClient);
 PowerController powerController(14, 16, mqttClient.client);
+#if enable_ota
 OtaManager otaManager;
+#endif
 SimpleTimer timer;
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
@@ -26,7 +31,9 @@ void setup() {
     mqttClient.setupMqtt();
 
     powerController.setup();
+#if enable_ota
     otaManager.setupOTA();
+#endif
 
     timer.setInterval(3000, []() { powerController.sensePower(); });
     timer.setInterval(120000, []() { mqttClient.publish(check_in_topic, "OK"); });
@@ -34,6 +41,8 @@ void setup() {
 
 void loop() {
     mqttClient.loop();
+#if enable_ota
     otaManager.handleOTA();
+#endif
     timer.run();
 }
